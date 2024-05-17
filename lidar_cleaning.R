03a5cbccafa4f8f216b7e4c134ebd00acb3ef13a
Nadeesha Illeperuma <nadeenotes9@gmail.com>
2024-05-16 22:34:11 -0400

    Update lidar_cleaning.R

--- lidar_cleaning.R
+++ lidar_cleaning.R
@@ -2,7 +2,13 @@
 
 # load required libraries
 library("rlas")
-setwd('/Users/13193/OneDrive - The University of South Dakota/Desktop/lidar/Gavins/las2020/')
+# setwd('/Users/13193/OneDrive - The University of South Dakota/Desktop/lidar/Gavins/las2020/')
+
+# Prompting the user for the working directory
+working_dir <- readline(prompt = "Please enter the working directory path: ")
+
+# Set the working directory
+setwd(working_dir)
 
 # select * = any character before dot, $=indicates end of the las file
 # list.files function to select only the las files from the folder
@@ -22,9 +28,16 @@ for (i in 1:length(file_list)){
   subset_las2=subset(subset_las1, Z>lowbound)
   # creating corrected las data
   lasheader = header_create(subset_las2)
-  cor_subset=write.las(paste('/Users/13193/OneDrive - The University of South Dakota/Desktop/lidar/Gavins/las2020/cor_las2020',paste("cor_",file_list[i], sep = ""),sep=""),lasheader, subset_las2)
-  #Sys.sleep(1)
+   output_file <- file.path(working_dir, "outputdir", paste("cor_", file_list[i], sep = ""))
+  
+  # Ensure the output directory exists
+  if (!dir.exists(file.path(working_dir, "outputdir"))) {
+    dir.create(file.path(working_dir, "outputdir"))
+  }
+  
+  cor_subset <- write.las(output_file, lasheader, subset_las2)
 }
 
+print("Processing complete.")
 
 
